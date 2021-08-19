d2a8a83b9302652b23d723cfb4d6174373604231
Vignesh Thangamariappan <Vignesh.mariappan@full.co>
2021-08-19 12:41:11 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MainGameKit
+
+A description of this package.
---
+++ Tests/MainGameKitTests/MainGameKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MainGameKit
+
+final class MainGameKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MainGameKit().text, "Hello, World!")
+    }
+}
